326e413412ee3cd0c608ac381c6fbca935ef868f
R. Allan White <allan@catalyze.io>
2017-09-06 11:12:27 -0700

    Trying to get CF to run

--- config.rb
+++ config.rb
@@ -32,3 +32,30 @@ configure :build do
     html.remove_intertag_spaces = true
   end
 end
+
+# Contentful configuration
+activate :contentful do |f|
+    f.space = { site: "189dvqdsjh46"}
+    f.access_token = "9abdf5de79abc1e1cdc2cf25f3280641fd845a2f3fd5bad8222d1b457f20ba2d"
+    # f.use_preview_api = false
+    # f.all_entries = true
+    # f.all_entries_page_size = 10
+    f.content_types = {
+        # academy: "academy",
+        # blog: "blog",
+        # cta: "cta",
+        # caseStudy: "caseStudy",
+        # customers: "customers",
+        # discover: "discover",
+        # guides: "guides",
+        # leadershipGallery: "leadershipGallery",
+        # person: "person",
+        # podcastEntry: "podcastEntry",
+        # pressReleases: "pressReleases",
+        # products: "products",
+        # spotlights: "spotlights",
+        # quotes: "quotes",
+        # tags: "tags",
+        whitepapers: "whitepapers"
+    }
+end
