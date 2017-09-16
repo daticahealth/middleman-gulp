b9f16a9e142494671329557a7c1fc3fbfd1b4d42
R. Allan White <allan@catalyze.io>
2017-09-15 23:13:40 -0700

    getting it working, config'd

--- config.rb
+++ config.rb
@@ -33,29 +33,36 @@ configure :build do
   end
 end
 
+# activate :contentful do |f|
+#   f.access_token = "2e233e64dde3b688cc772a3eb080e7566570d277cbd4391a7fbec9e3217a6a9a"
+#   f.space = {links: "u4lyfh69psb2"}
+#   f.content_types = {link: "4D5cDE5S8Ee8E8AKS6uswe"}
+#   f.cda_query = {content_type: "4D5cDE5S8Ee8E8AKS6uswe"}
+# end
+
 # Contentful configuration
 activate :contentful do |f|
-    f.space = { site: "189dvqdsjh46"}
-    f.access_token = "9abdf5de79abc1e1cdc2cf25f3280641fd845a2f3fd5bad8222d1b457f20ba2d"
-    # f.use_preview_api = false
-    # f.all_entries = true
-    # f.all_entries_page_size = 10
+    f.space = { site: '189dvqdsjh46'}
+    f.access_token = '9abdf5de79abc1e1cdc2cf25f3280641fd845a2f3fd5bad8222d1b457f20ba2d'
+    f.use_preview_api = false
+    f.all_entries = true
+    f.client_options = { max_include_resolution_depth: 1 }
     f.content_types = {
-        # academy: "academy",
-        # blog: "blog",
-        # cta: "cta",
-        # caseStudy: "caseStudy",
-        # customers: "customers",
-        # discover: "discover",
-        # guides: "guides",
-        # leadershipGallery: "leadershipGallery",
-        # person: "person",
-        # podcastEntry: "podcastEntry",
-        # pressReleases: "pressReleases",
-        # products: "products",
-        # spotlights: "spotlights",
-        # quotes: "quotes",
-        # tags: "tags",
-        whitepapers: "whitepapers"
+        # academy: 'academy',
+        # blog: 'blog',
+        # cta: 'cta',
+        # caseStudy: 'caseStudy',
+        # customers: 'customers',
+        # discover: 'discover',
+        # guides: 'guides',
+        # leadership_gallery: 'leadership_gallery',
+        # person: 'person',
+        # podcastEntry: 'podcastEntry',
+        # pressReleases: 'pressReleases',
+        # products: 'products',
+        # spotlights: 'spotlights',
+        # quotes: 'quotes',
+        # tags: 'tags',
+        whitepapers: 'whitepapers'
     }
 end
